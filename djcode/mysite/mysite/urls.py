9b7c6a5356f5594b8a98c99372be6eac18d5fc5e
Vaibhav <vaibhav.rbs@gmail.com>
2013-06-04 00:05:25 -0700

    my first Django Project commit

---
+++ djcode/mysite/mysite/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from mysite.views import hello, current_datetime,hours_ahead
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^hello/$',hello),
+    url(r'^time/$', current_datetime),
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+
+)
---
+++ djcode/mysite/mysite/views.py
@@ -0,0 +1,24 @@
+from django.template.loader import get_template
+from django.template import Context
+from django.http import HttpResponse
+import datetime
+
+def hello(request):
+    return HttpResponse("Hello World")
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    t = get_template('current_datetime.html')
+    html = t.render(Context({'current_date': now}))
+    return HttpResponse(html)
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
+
+
